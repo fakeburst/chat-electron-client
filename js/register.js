171e01fa45c07756e5d561d94e1c277e4edb2a52
fakeburst <lolguy322@gmail.com>
2017-06-27 03:59:22 +0300

    added registration

--- js/register.js
+++ js/register.js
@@ -1,23 +1,28 @@
 $(document).ready(function() {
-  
+
     $('#register').click(function() {
-        
-		// TO DO ACTION
-		
+        $.post("http://localhost:8080/login", $('form').serialize())
+            .done(function(data) {
+                if (!data.success) {
+                    alert(data.msg, "Error");
+                    return;
+                }
+                loginAction();
+            });
         return false;
     });
-	
-	
-	$('#back').click(function() {
-       window.location.replace("login.html");
-		event.preventDefault(); // disable normal form submit behavior
+
+
+    $('#back').click(function() {
+        window.location.replace("login.html");
+        event.preventDefault(); // disable normal form submit behavior
         return false;
     });
 });
 
 
 function loginAction() {
-	
+
     window.location.replace("login.html");
     event.preventDefault(); // disable normal form submit behavior
     return false; // prevent further bubbling of event
